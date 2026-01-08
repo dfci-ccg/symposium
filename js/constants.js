78c734948979fff8bd73bac430c93f6303acdbca
edwin <thai.et@gmail.com>
2026-01-08 15:09:52 -0500

    Dynamically fill in more links

--- js/constants.js
+++ js/constants.js
@@ -39,4 +39,22 @@ const DEADLINE_SHORT_DATE_STRING = formatFullDate(DEADLINE_DATE);
 const LIVESTREAM_TIME = addMinutesAndFormat(DEADLINE_DATE, 30);
 const LOCATION = 'Jimmy Fund Auditorium';
 const LOCATION_ADDRESS = '35 Binney Street, Boston, MA 02115';
+const ABOUT_HREF =
+  'https://www.dana-farber.org/research/departments-centers-and-labs/integrative-research-centers/center-for-cancer-genome-discovery/';
 const REGISTRATION_HREF = 'https://forms.gle/ZCZWY65VEQ45zsNp6';
+const AGENDA_HREF = `agendas/${DEADLINE_DATE.getFullYear()}.pdf`;
+const KEYNOTE_SPEAKERS = [
+  {
+    name: 'Fei Chen, PhD',
+    title:
+      'Associate Professor, Department of Stem Cell and Regenerative Biology ',
+    institute: 'Broad Institute; Harvard University',
+    img: 'img/guests/chen.png',
+  },
+  {
+    name: 'Alice Shaw, MD, PhD',
+    title: 'Chief, Strategic Partnerships ',
+    institute: 'Dana-Farber Cancer Institute',
+    img: 'img/guests/shaw.jpg',
+  },
+];
--- js/main.js
+++ js/main.js
@@ -3,6 +3,27 @@ function loadPartial(selector, partial) {
     $(selector).load(`/partials/${partial}`, resolve)
   );
 }
+/**
+ * Gives callback to easily inject into the the partial html
+ * @param {*} selector
+ * @param {*} partial
+ * @param {*} callback
+ * @returns
+ */
+function appendPartial(selector, partial, callback) {
+  return new Promise((resolve, reject) => {
+    $.get(`/partials/${partial}`)
+      .done((html) => {
+        const $el = $(html);
+        if (callback) {
+          callback($el);
+        }
+        $(selector).append($el);
+        resolve();
+      })
+      .fail(reject);
+  });
+}
 
 const navLoaded = loadPartial('#nav-container', 'nav.html');
 const bannerLoaded = loadPartial('#banner-container', 'banner.html');
@@ -10,11 +31,28 @@ const bannerLoaded = loadPartial('#banner-container', 'banner.html');
 /**
  * All dynamic filling should be done on this file in order to not have race conditions.
  */
-Promise.all([navLoaded, bannerLoaded]).then(() => {
-  $('.deadline-day').html(DEADLINE_DATE_STRING);
-  $('.deadline-short-day').html(DEADLINE_SHORT_DATE_STRING);
-  $('.deadline-year').html(DEADLINE_DATE.getFullYear());
-  $('.location').html(LOCATION);
-  $('.location-address').html(LOCATION_ADDRESS);
-  $('.livestream-time').html(LIVESTREAM_TIME);
-});
+Promise.all([navLoaded, bannerLoaded])
+  .then(() => {
+    return Promise.all(
+      KEYNOTE_SPEAKERS.map(({ img, institute, name, title }) =>
+        appendPartial('#keynote-speakers-container', 'keynote.html', ($el) => {
+          $el.find('.speaker').text(name);
+          $el.find('.title').text(title);
+          $el.find('.institute').text(institute);
+          $el.find('img.keynote-speaker').attr('src', img);
+        })
+      )
+    );
+  })
+  .then(() => {
+    $('.deadline-day').html(DEADLINE_DATE_STRING);
+    $('.deadline-short-day').html(DEADLINE_SHORT_DATE_STRING);
+    $('.deadline-year').html(DEADLINE_DATE.getFullYear());
+    $('.location').html(LOCATION);
+    $('.location-address').html(LOCATION_ADDRESS);
+    $('.livestream-time').html(LIVESTREAM_TIME);
+    // Href fill in
+    $('a.registration').attr('href', REGISTRATION_HREF);
+    $('a.agenda').attr('href', AGENDA_HREF);
+    $('a.about').attr('href', ABOUT_HREF);
+  });
