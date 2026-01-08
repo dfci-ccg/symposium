e6e4373abae414b4c5dd2c2360ceb61d28e72b8b
edwin <thai.et@gmail.com>
2026-01-08 14:29:14 -0500

    Fix broken html
    HTML file appears to have been not correctly made for some reason. I don't know
    if this was from me or just the previous authors.

--- js/constants.js
+++ js/constants.js
@@ -1,16 +1,16 @@
 function formatDateWithOrdinal(date) {
   const day = date.getDate();
 
-  let suffix = "th";
-  if (day % 10 === 1 && day !== 11) suffix = "st";
-  else if (day % 10 === 2 && day !== 12) suffix = "nd";
-  else if (day % 10 === 3 && day !== 13) suffix = "rd";
+  let suffix = 'th';
+  if (day % 10 === 1 && day !== 11) suffix = 'st';
+  else if (day % 10 === 2 && day !== 12) suffix = 'nd';
+  else if (day % 10 === 3 && day !== 13) suffix = 'rd';
 
   return `${day}${suffix}`;
 }
 function formatOnlyMonthDay(date) {
   const day = formatDateWithOrdinal(date);
-  const month = date.toLocaleString("en-US", { month: "long" });
+  const month = date.toLocaleString('en-US', { month: 'long' });
 
   return `${month} ${day}`;
 }
@@ -23,10 +23,10 @@ function addMinutesAndFormat(date, minutesToAdd) {
   const d = new Date(date.getTime());
   d.setMinutes(d.getMinutes() + minutesToAdd);
 
-  return d.toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true
+  return d.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
   });
 }
 
@@ -37,5 +37,6 @@ const DEADLINE_DATE_STRING = formatFullDate(DEADLINE_DATE);
  */
 const DEADLINE_SHORT_DATE_STRING = formatFullDate(DEADLINE_DATE);
 const LIVESTREAM_TIME = addMinutesAndFormat(DEADLINE_DATE, 30);
-const LOCATION = "Jimmy Fund Auditorium";
-const LOCATION_ADDRESS = "35 Binney Street, Boston, MA 02115";
+const LOCATION = 'Jimmy Fund Auditorium';
+const LOCATION_ADDRESS = '35 Binney Street, Boston, MA 02115';
+const REGISTRATION_HREF = 'https://forms.gle/ZCZWY65VEQ45zsNp6';
